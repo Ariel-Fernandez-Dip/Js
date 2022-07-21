7022446104318f882395118192e3f29b7996fc0a
Ariel Fernandez Dip <arielfernandezdip@hotmail.com>
2022-07-21 12:27:04 -0300

    third commit

---
+++ index.js
@@ -0,0 +1,92 @@
+const botonNumeros = document.getElementsByName( 'data-number');
+const botonOpera = document.getElementsByName('data-opera');
+const botonDelete = document.getElementsByName('data-delete')[0];
+const botonIgual = document.getElementsByName('data-igual')[0];
+let result = document.getElementById('result');
+let opeActual = '';
+let opeAnterior= '';
+let operacion = undefined;
+let i = 0;
+
+
+botonNumeros.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        agregarNumero(boton.innerText);
+    })
+});
+
+botonOpera.forEach(function(boton){
+    boton.addEventListener('click', function(){
+        SelecOperacion(boton.innerText);
+    })
+});
+
+botonIgual.addEventListener('click', function(){
+    calcular();
+    actualizarDisplay();
+});
+
+botonDelete.addEventListener('click', function(){
+    clear();
+    actualizarDisplay();
+});
+
+function SelecOperacion(op){
+    i = 0
+    if(opeActual === '') return;
+    if(opeAnterior !== ''){
+        calcular()
+    }
+
+    operacion = op.toString();
+    opeAnterior = opeActual;
+    opeActual = '';
+}
+
+function calcular(){
+    let calculo;
+    const anterior = parseFloat(opeAnterior);
+    const actual = parseFloat(opeActual);
+    if(isNaN(anterior) || isNaN(actual)) return;
+    switch(operacion){
+        case "+" :
+           calculo = anterior + actual;
+           break;
+        case "-":
+            calculo = anterior - actual;
+           break;
+        case "X":
+            calculo = anterior * actual;
+           break;
+        case "/":
+            calculo = anterior / actual;
+           break;        
+        default:
+           return;
+       }
+        i ++
+        opeActual = calculo;
+        operacion =undefined;
+        opeAnterior = '';
+    }
+
+function agregarNumero(num){
+    if (i === 1){
+        i = 0 
+        clear()
+    }
+    opeActual = opeActual.toString() + num.toString();
+    actualizarDisplay();
+};
+
+function clear(){
+    opeActual = '';
+    opeAnterior = '';
+    operacion = undefined;
+}
+
+function actualizarDisplay(){
+    result.value = opeActual;
+};
+
+clear();
